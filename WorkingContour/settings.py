af993b60fc86894c385fd629e1e362f67ff6a624
Виталий Сапрыкин <saprykin_va@MacBook-Air.local>
2026-07-14 15:05:32 +0300

    create three new apps (users, tasks, projects), install them in settings

--- WorkingContour/settings.py
+++ WorkingContour/settings.py
@@ -23,6 +23,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'projects',
+    'tasks'
 ]
 
 MIDDLEWARE = [
