52531d1e8a20d080d561a30c43046529a1e6e7fb
Виталий Сапрыкин <saprykin_va@MacBook-Air.local>
2026-07-15 16:08:07 +0300

    add Task model and Project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Project(models.Model):
+    class ProjectStatusChoices(models.TextChoices):
+        ACTIVE = 'ACTIVE'
+        ARCHIVED = 'ARCHIVED'
+
+    name = models.CharField(max_length=250)
+    description = models.TextField(
+        blank=True
+    )
+    status = models.CharField(
+        choices=ProjectStatusChoices.choices,
+        max_length=10,
+        default=ProjectStatusChoices.ACTIVE
+    )
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+from projects.models import Project
 
-# Create your models here.
+
+class Task(models.Model):
+    class TaskStatusChoices(models.TextChoices):
+        TODO = 'TODO'
+        IN_PROGRESS = 'IN_PROGRESS'
+        DONE = 'DONE'
+
+    title = models.CharField(max_length=250)
+    description = models.TextField(
+        blank=True
+    )
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+    )
+    status = models.CharField(
+        choices=TaskStatusChoices.choices,
+        max_length=15,
+        default=TaskStatusChoices.TODO
+    )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
